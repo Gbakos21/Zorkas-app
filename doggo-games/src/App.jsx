ca2d8f849df4ed4db6031f0064b5c4bd59b7f8cf
gbakos21 <gbakos21@gmail.com>
2025-09-06 10:18:19 +0200

    dep 10

--- doggo-games/src/App.jsx
+++ doggo-games/src/App.jsx
@@ -1,10 +1,8 @@
 import "./App.css";
 import DoggoGames from "./DoggoGames";
 
-const initialPhotos = Array.from({ length: 16 }, (_, i) => `/maci${i + 1}.jpg`);
-
 function App() {
-  return <DoggoGames initialPhotos={initialPhotos} />;
+  return <DoggoGames />;
 }
 
 export default App;
